bd264ba70ed4c7de2a848c3dd88409c08e1d13b6
Mili Roy <mr3964@columbia.edu>
2019-11-24 00:36:22 +0000

    Inserted code for exporting csv

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -2,18 +2,26 @@
   
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Sighting #as "something" needed?
+from django.http import HttpResponse
 
+
+import csv
+import sys
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Export data to csv'
+
     def add_arguments(self, parser):
-        parser.add_argument('poll_ids', nargs='+', type=int)
+        parser.add_argument('file_path', help='filepath name')
+
     def handle(self, *args, **options):
-        for poll_id in options['poll_ids']:
-            try:
-                poll = Poll.objects.get(pk=poll_id)
-            except Poll.DoesNotExist:
-                raise CommandError('Poll "%s" does not exist' % poll_id)
-            poll.opened = False
-            poll.save()
-            self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        meta = Sighting._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={file_path}.csv'.format(meta) #need to check if this is the right way to put in file path name
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for instance in Sighting.objects.all().select_related(): 
+            writer.writerow([unicode(getattr(instance, f)).encode('utf-8') for f in field_names])
